d930b02b5f9aad21e6f44d9ded949d236fdd08f7
Aleksandra <abaibik@gmail.com>
2023-09-26 16:44:19 +0200

    add frontend

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,29 @@
+import useSWR from "swr";
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  const birthday = new Date(data.birthday).toString();
+
+  return (
+    <>
+      <h1>
+        {data.firstName} {data.lastName}
+      </h1>
+      <p>birthday: {birthday}</p>
+      <p>
+        twitter: {data.twitter}, geohash: {data.geohash}
+      </p>
+    </>
+  );
 }
